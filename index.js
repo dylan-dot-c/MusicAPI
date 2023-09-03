6a91095be3e24fff1c97ff0cfcb96675ce07cdb4
Dylan Heslop <heslopd23@gmail.com>
2023-09-02 21:51:36 -0400

    updated port number

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-const express = require('express')
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
 
-const songRouter = require('./routes/songRouter.js')
-const playListRouter = require('./routes/playlistRouter.js')
+const songRouter = require("./routes/songRouter.js");
+const playListRouter = require("./routes/playlistRouter.js");
 
-const db = require("./models")
+const db = require("./models");
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-db.sequelize.sync({alter: true}).then( (req) => {
-    app.listen(3000, () => {
-        console.log("SERVER IS UP")
-    })
-})
+db.sequelize.sync({ alter: true }).then((req) => {
+  app.listen(6857, () => {
+    console.log("SERVER IS UP");
+  });
+});
 
-app.use('/songs', songRouter)
-app.use('/playlists', playListRouter)
+app.use("/songs", songRouter);
+app.use("/playlists", playListRouter);
 
 app.get("/", (req, res) => {
-    res.send({msg: "Welcome to my api: Docc https://documenter.getpostman.com/view/26781479/2s93sdYBXg"})
-})  
+  res.send({
+    msg: "Welcome to my api: Docc https://documenter.getpostman.com/view/26781479/2s93sdYBXg",
+  });
+});
