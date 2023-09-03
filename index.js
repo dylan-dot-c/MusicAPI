a9247981b874fea4c5ff15666097b38b6c6e3c47
Dylan Heslop <heslopd23@gmail.com>
2023-09-02 22:19:49 -0400

    Fixing the port Issue

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
+const dotenv = require("dotenv");
 const express = require("express");
 const cors = require("cors");
 
 const songRouter = require("./routes/songRouter.js");
 const playListRouter = require("./routes/playlistRouter.js");
-
 const db = require("./models");
 
 const app = express();
 
+dotenv.config();
+
+const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 
 db.sequelize.sync({ alter: true }).then((req) => {
-  app.listen(6857, "0.0.0.0", () => {
+  app.listen(port, () => {
     console.log("SERVER IS UP");
+    console.log("SERVER IS on ", port);
   });
 });
 
